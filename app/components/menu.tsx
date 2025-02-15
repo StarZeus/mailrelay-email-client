0202019fe879a7550103acb880948cfd943e0da0
Vinoth Kirubakaran <vinothefifthstar@hotmail.com>
2025-02-15 05:59:33 -0700

    fixed icons

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,4 +1,4 @@
-import { Menu, Star, FileText, Send, Check, Trash } from 'lucide-react';
+import { Menu, Star, FileText, Send, Check, Trash, Mail, MailCheck, MailQuestion, MailCheckIcon, MailOpen, Settings } from 'lucide-react';
 import {
   Sheet,
   SheetContent,
@@ -9,34 +9,34 @@ import Link from 'next/link';
 
 export function NavMenu() {
   return (
-    <nav className="flex flex-col space-y-4">
+    <nav className="flex flex-col">
       <Link
         href="/f/inbox"
-        className="flex items-center space-x-1 text-gray-700 hover:bg-gray-100 p-2 pl-4 rounded"
+        className="flex items-center space-x-1 text-gray-700 hover:bg-gray-100 px-6 py-4 rounded"
       >
-        <Menu size={20} />
-        <span>Inbox</span>
+        <Mail size={20} />
+        <span className="pl-2">Inbox</span>
       </Link>
       <Link
         href="/f/filters"
-        className="flex items-center space-x-1 text-gray-700 hover:bg-gray-100 p-2 pl-4 rounded"
+        className="flex items-center space-x-1 text-gray-700 hover:bg-gray-100 px-6 py-4 rounded"
       >
-        <FileText size={20} />
-        <span>Filters & Actions</span>
+        <Settings size={20} />
+        <span className="pl-2">Filters & Actions</span>
       </Link>
       <Link
         href="/f/sent"
-        className="flex items-center space-x-1 text-gray-700 hover:bg-gray-100 p-2 pl-4 rounded"
+        className="flex items-center space-x-1 text-gray-700 hover:bg-gray-100 px-6 py-4 rounded"
       >
         <Send size={20} />
-        <span>Sent Mail</span>
+        <span className="pl-2">Sent Mail</span>
       </Link>
       <Link
         href="/f/processed"
-        className="flex items-center space-x-1 text-gray-700 hover:bg-gray-100 p-2 pl-4 rounded"
+        className="flex items-center space-x-1 text-gray-700 hover:bg-gray-100 px-6 py-4 rounded"
       >
-        <Check size={20} />
-        <span>Processed</span>
+        <MailCheck size={20} />
+        <span className="pl-2">Processed</span>
       </Link>
     </nav>
   );
